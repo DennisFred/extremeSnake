4ca6fe00acae70608fb7e6013ca3f72618e98136
DennisFred <dschilling@karlshochschule.de>
2017-06-10 17:18:01 +0200

    Initial Commit

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  extremeSnake
+//
+//  Created by Dennis Schilling on 10.06.17.
+//  Copyright © 2017 DennisSchilling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
