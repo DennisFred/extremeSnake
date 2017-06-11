2e79486291495ca5846fbf81d4823d9a6b44181c
DennisFred <dschilling@karlshochschule.de>
2017-06-11 06:53:11 +0200

    init with coder suport

--- Shared/GameScene.swift
+++ Shared/GameScene.swift
@@ -31,7 +31,7 @@ class GameScene: SKScene {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     class func newGameScene() -> GameScene {
